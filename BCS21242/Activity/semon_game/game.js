68757e9a1ec8ff1e9bed471705ce9b90afb7ef90
Ahmad Amjad <160136773+AHMADAMJAD0@users.noreply.github.com>
2024-10-07 23:17:13 +0500

    Add files via upload
    web project

---
+++ BCS21242/Activity/semon_game/game.js
@@ -0,0 +1,94 @@
+$(document).ready(function(){
+    // $("p").click(function(){
+    //   console.log("my name is ahmad ");
+    //   $(this).hide();
+    // })
+
+    // $("h1").click(function(){
+    //   console.log("my name is ahmad ");
+    //   $(this).hide();
+    // })
+    
+   // Set multiple CSS properties for all paragraphs
+// $("p").css({
+//   "color": "red",
+//   "font-size": "18px",
+//   "background-color": "yellow"
+// });
+
+// function helloWorld() {
+//   console.log("Hello, world!");
+// }
+
+// $("div").addClass("home")
+
+// $("h4").click(function(){
+//   $("div").hasClass("qwert")
+// })
+// Check if the element with id "myElement" has the class "highlight"
+// if ($("div").hasClass("home")) {
+//   console.log("The element has the class 'highlight'.");
+// } else {
+//   console.log("The element does not have the class 'highlight'.");
+// }
+
+
+// Get the text content of the first <p> element
+// var textContent = $("p").text();
+// console.log(textContent);
+
+
+
+// Set the HTML content of all <div> elements
+// $("div").html("<strong>This is bold text.</strong>");
+
+// var value = $().attr(attributeName);
+
+// Set the text content of all <p> elements
+// $("p").text("This is new text.");
+
+
+// $("#but").click(function(){
+//   $("#wiki").toggle(1000);
+// })
+
+
+// $("#but").click(function(){
+//   $("#wiki").fadeOut(1000); //slow removing 
+// })
+
+
+
+// $("#but").click(function(){
+//   $("#wiki").fadeIn(1000); //slow removing 
+// })
+
+
+
+// $("#but").click(function(){
+//   $("#wiki").fadeToggle(1000); //slow removing 
+// })
+
+
+
+// $("#but").click(function(){
+//   $("#wiki").fadeTo(1000); //slow removing 
+// })
+
+
+
+
+// $("button").click(function() {
+//   // 'this' refers to the clicked button
+//   $(this).text("Clicked!");
+// });
+
+
+$("button").click(function() {
+  alert("Button clicked!");
+});
+   
+
+
+  }
+);
---
+++ BCS21242/Activity/semon_game/script.js
@@ -0,0 +1,30 @@
+// 4 piller of dom 
+
+// selection of an alement 
+
+//  {var a = document.querySelector("h1")
+// console.log(a)}
+
+
+// changing html
+
+
+//var a = document.querySelector("h1")
+//a.innerHTML = " changed html"
+
+// changing css 
+
+//var a = document.querySelector("h1")
+//a.style.color = "yellow"
+//a.style.backgroundColor = "black"
+
+
+
+
+var a = document.querySelector("h1")
+ a.addEventListener("click",function(){
+a.innerHTML = " hum na baz ay gy muhbat say "
+a.style.color = "blue"
+a.style.backgroundColor = "pink"
+
+ })
